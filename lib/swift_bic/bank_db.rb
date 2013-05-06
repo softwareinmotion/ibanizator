bda12e2cf0fdd452f82362f9c6ad6f0acbd3c7ef
Christoph Stettner <christoph.stettner@softwareinmotion.de>
2013-05-06 12:04:35 +0200

    adds exception handling for bic and bank name searching

--- lib/swift_bic/bank_db.rb
+++ lib/swift_bic/bank_db.rb
@@ -1,9 +1,13 @@
 require 'sqlite3'
+require_relative '../errors/invalid_bank_code_error'
+require_relative '../errors/bank_not_found_error'
 
 module SwiftBic
   class BankDb
-    def initialize bank_number
-      @bank_data = load_table_row bank_number
+    def initialize bank_code
+      validate_bank_code bank_code
+      @bank_data = load_table_row bank_code
+      raise BankNotFoundError unless @bank_data
     end
 
     def bank_name
@@ -14,13 +18,20 @@ module SwiftBic
       @bank_data[2]
     end
 
+    def validate_bank_code bank_code
+      if bank_code.length == 8
+        return true
+      else
+        raise InvalidBankCodeError
+      end
+    end
 
     private
 
-    def load_table_row bank_number
+    def load_table_row bank_code
       begin
         db = SQLite3::Database.open(File.expand_path("../../../db/blz.db", __FILE__))
-        row = db.execute("SELECT * FROM blz WHERE blz = '#{bank_number}'")
+        row = db.execute("SELECT * FROM blz WHERE blz = '#{bank_code}'")
       rescue SQLite3::Exception => e
         throw e
       ensure
--- spec/swift_bic/bank_db_spec.rb
+++ spec/swift_bic/bank_db_spec.rb
@@ -1,7 +1,18 @@
 require_relative '../../lib/swift_bic/bank_db'
+require_relative '../../lib/errors/invalid_bank_code_error'
 
 describe SwiftBic::BankDb do
   let(:bank_db) { SwiftBic::BankDb.new '10000000' }
+
+  describe '#initialize' do
+    context 'given there is no bank for the bank code' do
+      it 'raises a bank not found exception' do
+        expect {
+          SwiftBic::BankDb.new '12345678'
+        }.to raise_error(BankNotFoundError)
+      end
+    end
+  end
 
   describe '#bank_name' do
     it 'returns the bank name' do
@@ -12,6 +23,22 @@ describe SwiftBic::BankDb do
   describe '#bic' do
     it 'returns the bic' do
       expect(bank_db.bic).to eq('MARKDEF1100')
+    end
+  end
+
+  describe '#validate_bank_code' do
+    context 'given valid german bank code' do
+      it 'returns true' do
+        expect(bank_db.validate_bank_code('10000000')).to be_true
+      end
+    end
+
+    context 'given invalid bank code' do
+      it 'raises an invalid bank code exception' do
+        expect {
+          bank_db.validate_bank_code('123')
+        }.to raise_error(InvalidBankCodeError)
+      end
     end
   end
 end
