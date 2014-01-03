960372d065a570834a2a8e9be201bd7c38ef2e80
Martin Honermeyer <martin.honermeyer@zweitag.de>
2014-01-03 12:37:41 +0100

    Replace sqlite with official txt file from Bundesbank
    refs #3

--- lib/swift_bic/bank_db.rb
+++ lib/swift_bic/bank_db.rb
@@ -1,21 +1,30 @@
-require 'sqlite3'
 require_relative '../errors/invalid_bank_code_error'
 require_relative '../errors/bank_not_found_error'
 
 module SwiftBic
   class BankDb
+
+    BANKS = {}.tap do |banks|
+      File.open('db/blz.txt', 'r').each_line do |line|
+        code, _, _, _, _, name, _, bic = line.unpack 'A8A1A58A5A35A27A5A11'
+        next if bic.empty?
+        name.force_encoding('iso-8859-1').encode!('utf-8')
+        banks[code] = [name, bic]
+      end
+    end
+
     def initialize bank_code
       validate_bank_code bank_code
-      @bank_data = load_table_row bank_code
+      @bank_data = BANKS[bank_code]
       raise BankNotFoundError unless @bank_data
     end
 
     def bank_name
-      @bank_data[1]
+      @bank_data[0]
     end
 
     def bic
-      @bank_data[2]
+      @bank_data[1]
     end
 
     def validate_bank_code bank_code
@@ -24,20 +33,6 @@ module SwiftBic
       else
         raise InvalidBankCodeError
       end
-    end
-
-    private
-
-    def load_table_row bank_code
-      begin
-        db = SQLite3::Database.open(File.expand_path("../../../db/blz.db", __FILE__))
-        row = db.execute("SELECT * FROM blz WHERE blz = '#{bank_code}'")
-      rescue SQLite3::Exception => e
-        throw e
-      ensure
-        db.close
-      end
-      row[0]
     end
   end
 end
